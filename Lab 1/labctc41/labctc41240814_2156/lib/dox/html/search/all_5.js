c79f4f157db99c91cdf8c4a04a66ba44dbc1ef20
DannxC <daniel_cavassani@hotmail.com>
2024-09-15 12:53:40 -0300

    changed directory tree

--- Lab 1/labctc41/labctc41240814_2156/lib/dox/html/search/all_5.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['initializeprinter_16',['initializePrinter',['../log_8c.html#a0ba66d28f34aadc7b447856f180dec55',1,'initializePrinter(const char *path, const char *baseName, FileDestination files2open):&#160;log.c'],['../log_8h.html#a0ba66d28f34aadc7b447856f180dec55',1,'initializePrinter(const char *path, const char *baseName, FileDestination files2open):&#160;log.c']]]
-];
--- Lab 1/labctc41/labctc41240814_2156/lib/dox/html/search/files_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['log_2ec_32',['log.c',['../log_8c.html',1,'']]],
-  ['log_2eh_33',['log.h',['../log_8h.html',1,'']]]
-];
